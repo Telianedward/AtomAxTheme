3b9db0171960641140d8be1ed49055a25270ce2c
edward <Telianedward@icloud.com>
2022-09-21 01:44:07 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/colors.js
+++ src/colors.js
@@ -6,6 +6,6 @@ const _l = require("@primer/primitives/dist/json/colors/light.json"),
       _dD = require("@primer/primitives/dist/json/colors/dark_dimmed.json"),
       _dHC  = require("@primer/primitives/dist/json/colors/dark_high_contrast.json"),
       _dC = require("@primer/primitives/dist/json/colors/dark_colorblind.json"),
-      _dT = require("@primer/primitives/dist/json/colors/dark_tritanopia.json")
+      _dT = require("@primer/primitives/dist/json/colors/dark_tritanopia.json"),
       _gC = (t) => {return t === "l"?_l:(t === "lhc"?_lHC:(t === "lc"?_lC:(t === "lt"?_lT:(t === "d"?_d:(t === "dd"?_dD:(t === "dhc"?_dHC:(t === "dc"?_dC:(t === "dt"?_dT:false))))))))}
       module.exports = {_gC};
