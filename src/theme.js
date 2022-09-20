939f0bb91e62bfb54d729e46f2188abef59dc03c
edward <Telianedward@icloud.com>
2022-09-21 01:52:55 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,13 @@
 
-const _l = require("@primer/primitives/dist/json/colors/light.json"),
-_lHC = require("@primer/primitives/dist/json/colors/light_high_contrast.json"),
-_lC = require("@primer/primitives/dist/json/colors/light_colorblind.json"),
-_lT = require("@primer/primitives/dist/json/colors/light_tritanopia.json"),
-_d = require("@primer/primitives/dist/json/colors/dark.json"),
-_dD = require("@primer/primitives/dist/json/colors/dark_dimmed.json"),
-_dHC  = require("@primer/primitives/dist/json/colors/dark_high_contrast.json"),
-_dC = require("@primer/primitives/dist/json/colors/dark_colorblind.json"),
-_dT = require("@primer/primitives/dist/json/colors/dark_tritanopia.json"),
+const _l = require("./colors/light.json"),
+_lHC = require("./colors/light_high_contrast.json"),
+_lC = require("./colors/light_colorblind.json"),
+_lT = require("./colors/light_tritanopia.json"),
+_d = require("./colors/dark.json"),
+_dD = require("./colors/dark_dimmed.json"),
+_dHC  = require("./colors/dark_high_contrast.json"),
+_dC = require("./colors/dark_colorblind.json"),
+_dT = require("./colors/dark_tritanopia.json"),
 _gC = (t) => {return t === "l"?_l:(t === "lhc"?_lHC:(t === "lc"?_lC:(t === "lt"?_lT:(t === "d"?_d:(t === "dd"?_dD:(t === "dhc"?_dHC:(t === "dc"?_dC:(t === "dt"?_dT:false))))))))}
 
 
