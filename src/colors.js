5cf5ac63b92c0af54bcf3133c7e6251cf6b7b2f4
edward <Telianedward@icloud.com>
2022-09-21 12:59:40 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/colors.js
+++ src/colors.js
@@ -1,11 +1,11 @@
-import _l from "../node_modules/@primer/primitives/dist/json/colors/light.json";
-import _lHC from "../node_modules/@primer/primitives/dist/json/colors/light_high_contrast.json";
-import _lC from "../node_modules/@primer/primitives/dist/json/colors/light_colorblind.json";
-import _lT from "../node_modules/@primer/primitives/dist/json/colors/light_tritanopia.json";
-import _d from "../node_modules/@primer/primitives/dist/json/colors/dark.json";
-import _dD from "../node_modules/@primer/primitives/dist/json/colors/dark_dimmed.json";
-import _dHC from "../node_modules/@primer/primitives/dist/json/colors/dark_high_contrast.json";
-import _dC from "../node_modules/@primer/primitives/dist/json/colors/dark_colorblind.json";
-import _dT from "../node_modules/@primer/primitives/dist/json/colors/dark_tritanopia.json";
-      _gC = (t) => {return t === "l"?_l:(t === "lhc"?_lHC:(t === "lc"?_lC:(t === "lt"?_lT:(t === "d"?_d:(t === "dd"?_dD:(t === "dhc"?_dHC:(t === "dc"?_dC:(t === "dt"?_dT:false))))))))}
-export  {_gC};
+import _l from "../node_modules/@primer/primitives/dist/json/colors/light.json" assert { type: "json" };
+import _lHC from "../node_modules/@primer/primitives/dist/json/colors/light_high_contrast.json" assert { type: "json" };
+import _lC from "../node_modules/@primer/primitives/dist/json/colors/light_colorblind.json" assert { type: "json" };
+import _lT from "../node_modules/@primer/primitives/dist/json/colors/light_tritanopia.json" assert { type: "json" };
+import _d from "../node_modules/@primer/primitives/dist/json/colors/dark.json" assert { type: "json" };
+import _dD from "../node_modules/@primer/primitives/dist/json/colors/dark_dimmed.json" assert { type: "json" } ;
+import _dHC from "../node_modules/@primer/primitives/dist/json/colors/dark_high_contrast.json" assert { type: "json" } ;
+import _dC from "../node_modules/@primer/primitives/dist/json/colors/dark_colorblind.json" assert { type: "json" };
+import _dT from "../node_modules/@primer/primitives/dist/json/colors/dark_tritanopia.json" assert { type: "json" };
+    const  _gC = (t) => {return t === "l"?_l:(t === "lhc"?_lHC:(t === "lc"?_lC:(t === "lt"?_lT:(t === "d"?_d:(t === "dd"?_dD:(t === "dhc"?_dHC:(t === "dc"?_dC:(t === "dt"?_dT:false))))))))}
+export default  _gC;
