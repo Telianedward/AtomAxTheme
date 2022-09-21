17034b1f96cadac6838a76623fcf380d6e31bd5c
edward <Telianedward@icloud.com>
2022-09-21 17:17:05 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/theme.js
+++ src/theme.js
@@ -47,11 +47,13 @@ const   hslToRgb = (h, s, l) => {
             if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
           return p;
       }
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      console.log(h,'h - ')
+      console.log(s,'s - ')
+      console.log(l,'l - ')
+
+    var q = l/100 < 0.5 ? (l * (1 + s) ):( l + s - l * s);
     var p = 2 * l - q;
-          console.log(h,'h - ')
-          console.log(s,'s - ')
-          console.log(l,'l - ')
+  
           r = hue2rgb(p, q, h + 1/3);
           g = hue2rgb(p, q, h);
           b = hue2rgb(p, q, h - 1/3);
