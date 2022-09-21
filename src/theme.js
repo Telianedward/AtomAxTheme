3673880ea2a3db188a033ba928ea0d5e97b7ea3b
edward <Telianedward@icloud.com>
2022-09-21 14:42:08 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/theme.js
+++ src/theme.js
@@ -27,24 +27,41 @@ const cycle = (val) => {
   }
   return val;
 }
+   hslToRgb = (h, s, l) => {
+  var r, g, b;
 
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
 const hsl_ = (hue, saturation, luminosity)=> {
 
   console.log(hue ,' hue ----- ')
   console.log(saturation ,' saturation ----- ')
   console.log(luminosity ,' luminosity ----- ')
-  hue = cycle(hue);
-  saturation = min(max(saturation, 100), 0);
-  luminosity = min(max(luminosity, 100), 0);
-
-  saturation /= 100;
-  luminosity /= 100;
-  console.log(hue ,' hue ----- ')
-  console.log(saturation ,' saturation ----- ')
-  console.log(luminosity ,' luminosity ----- ')
-  const rgb = _toRgb(hue, saturation, luminosity);
-
-  return `#${rgb.map(n => (256 + n).toString(16).substr(-2)).join("")}`;
+  let k = hslToRgb(hue, saturation, luminosity)
+  console.log(k[0] ,' hue ----- + ')
+  console.log(k[1] ,' saturation ----- +  ')
+  console.log(k[2] ,' luminosity ----- + ')
+  return _cl.g(k[0],k[1],k[2])
 }
 const _cl = {
   c: ( c ) => {
